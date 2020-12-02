c169d8145b1fd02dc6700aa18497a88862b4ae95
Serg M <elcrmo@icloud.com>
2020-12-02 17:07:39 +0300

    created buttons, added more redux logic

--- client/components/taskList.js
+++ client/components/taskList.js
@@ -3,6 +3,7 @@ import { useSelector, useDispatch } from 'react-redux'
 import { useParams } from 'react-router-dom'
 import Head from './head'
 import { getTasks } from '../redux/reducers/tasks'
+import Task from './common/task'
 
 const TaskList = () => {
   const dispatch = useDispatch()
@@ -16,12 +17,16 @@ const TaskList = () => {
       <Head title="TaskList" />
       <div className="flex items-center justify-center h-screen">
         <div className="bg-indigo-800 hover:text-red-500 text-white font-bold rounded-lg border shadow-lg p-10">
-          {JSON.stringify(tasks)}
+          {tasks.map((item) => (
+            <Task taskData={item} category={category} key={item.taskId} />
+          ))}
         </div>
       </div>
     </div>
   )
 }
+
+
 
 TaskList.propTypes = {}
 
--- client/redux/reducers/tasks.js
+++ client/redux/reducers/tasks.js
@@ -1,15 +1,32 @@
 import axios from 'axios'
 
 const GET_TASKS = 'GET_TASKS'
+const UPDATE_SMTH = 'UPDATE_SMTH'
+const CHANGE_STATUS = 'CHANGE_STATUS'
 
 const initialState = {
-  listOfTasks: ['1232343', '12324']
+  listOfTasks: []
 }
 
 export default (state = initialState, action) => {
   switch (action.type) {
-     case GET_TASKS: {
-      return { ...state, listOfTasks: action.listOfTasks }
+    case GET_TASKS: {
+      return {
+        ...state,
+        listOfTasks: action.listOfTasks
+      }
+    }
+    case CHANGE_STATUS: {
+      return {
+        ...state,
+        listOfTasks: action.changedStatus
+      }
+    }
+    case UPDATE_SMTH: {
+      return {
+        ...state,
+        listOfTasks: [...state.listOfTasks, action.new]
+      }
     }
     default:
       return state
@@ -18,9 +35,31 @@ export default (state = initialState, action) => {
 
 export function getTasks(category) {
   return (dispatch) => {
-    axios(`/api/v1/tasks/${category}`)
-      .then(({ data }) => {
-        dispatch({ type: GET_TASKS, listOfTasks: data })
-      })
-   }
+    axios(`/api/v1/tasks/${category}`).then(({ data }) => {
+      dispatch({ type: GET_TASKS, listOfTasks: data })
+    })
+  }
 }
+
+export function changeStatus(category, id, status) {
+  return (dispatch, getState) => {
+    const store = getState()
+    const { listOfTasks } = store.tasks
+    const changedStatus = listOfTasks.map((item) =>
+      item.taskId === id ? { ...item, status} : { ...item }
+    )
+    axios({
+      method: 'patch',
+      url: `/api/v1/tasks/${category}/${id}`,
+      data: {
+        status: 'in progress'
+      }
+    })
+    dispatch({ type: CHANGE_STATUS, changedStatus })
+  }
+}
+
+export function updateSmth() {
+  return { type: UPDATE_SMTH, new: 'blabla' }
+}
+
